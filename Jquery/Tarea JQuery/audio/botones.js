15c547047be8ede96caf2c13cbb401ddf43fced3
Oscar Portillo <oscar94.oscar@hotmail.com>
2019-02-11 21:10:19 +0100

    añadir a la maqueta jQuery

--- Jquery/Tarea JQuery/audio/botones.js
+++ Jquery/Tarea JQuery/audio/botones.js
@@ -1,4 +1,59 @@
-window.onload = function () {
+$(document).ready(function () {
+
+    //$("#mostrarOcultarAudio").hide();
+    console.log("documento cargado");
+    botones();
+    audioEscondido(); //oculto los elementos para poder interactuar con ellos
+    ocultarAudio(); //uso el hide()
+    mostrarAudio(); //uso el show()
+    verVideo();
+    ocultarVideo();
+});
+
+function verVideo() {
+    $(".verVideo").click(function () {
+        $("#videoInformativo").fadeIn()
+        $(".verVideo").hide()
+        $(".ocultarVideo").show()
+        
+    })
+}
+
+function ocultarVideo() {
+    $(".ocultarVideo").click(function () {
+        $("#videoInformativo").fadeOut()
+        $(".verVideo").show()
+        $(".ocultarVideo").hide()
+    })
+
+}
+
+function ocultarAudio() {
+    $(".ocultarAudio").click(function () {
+        $("#mostrarOcultarAudio").hide()
+        $(".mostrarAudio").show()
+        $(".ocultarAudio").hide()
+    });
+}
+
+function mostrarAudio() {
+    $(".mostrarAudio").click(function () {
+        $("#mostrarOcultarAudio").show()
+        $(".ocultarAudio").show()
+        $(".mostrarAudio").hide()
+    });
+}
+
+function audioEscondido() {
+    $("#mostrarOcultarAudio").hide()
+    $(".ocultarAudio").hide()
+    $("#videoInformativo").hide()
+    $(".ocultarVideo").hide()
+    $("#ocultarVideoYouTube").hide()
+
+}
+
+function botones() {
     var play = document.getElementById("play");
     var pause = document.getElementById("pause");
     var more = document.getElementById("more");
@@ -12,7 +67,7 @@ window.onload = function () {
     pause.addEventListener("click", function (e) {
         x.pause();
     });
-    
+
     more.addEventListener("click", function (e) {
         x.volume += 0.1;
     });
